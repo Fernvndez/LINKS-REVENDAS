4862573c6f031eda717f02f85ca97cf77a959011
Leonardo Fernandez <159663804+Fernvndez@users.noreply.github.com>
2024-12-05 10:11:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-base: '/LINKS-REVENDAS/'
 export default defineConfig({
+  base: '/LINKS-REVENDAS/', // Adicione a base correta aqui
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
